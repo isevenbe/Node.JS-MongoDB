15ff3b23a3a6101ecb56c6c21f8512be3285bdbd
isevenbe <florentin.lefort@gmail.com>
2018-12-12 11:44:58 +0100

    Allow

--- routes/modelRoutes.js
+++ routes/modelRoutes.js
@@ -2,6 +2,13 @@ const studentModels = require("../models/MyModel")
 
 module.exports = (app) => {
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
   app.get("/student", (req, res) => {
     studentModels.find((err, student) => {
       if (err)
