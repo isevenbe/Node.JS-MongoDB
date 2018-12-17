55fee57844c16fa69443a1105b6c5e5d4243c46f
isevenbe <florentin.lefort@gmail.com>
2018-12-17 11:51:18 +0100

    api path + token

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,23 +1,6 @@
 const usersModel = require("../models/usersModel");
 const jwt = require('jsonwebtoken');
 module.exports = (app) => {
-  app.get('/setup', function (req, res) {
-
-    // create a sample user
-    let florentin = new usersModel({
-      name: 'Florentin',
-      password: 'password',
-      admin: true
-    });
-
-    // save the sample user
-    florentin.save(function (err) {
-      if (err) throw err;
-
-      console.log('User saved successfully');
-      res.json({ success: true });
-    });
-  });
 
   app.get("/users", (req, res) => {
     usersModel.find({}, (err, users) => {
@@ -62,5 +45,7 @@ module.exports = (app) => {
 
     });
   });
+
+  app.use('/api', apiRoutes);
 
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const url = process.env.MONGOLAB_URI;
 const config = require('./config');
+const apiRoutes = express.Router(); 
 
 // Connection to MongoDB
 mongoose.connect(url, { useNewUrlParser: true });
@@ -12,7 +13,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => { console.log("Connection to Mongoose Success..."); });
 
 // Load my app
-let app = express();
+let app = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,17 +24,49 @@ require("./routes/modelRoutes")(app);
 require("./routes/usersRoutes")(app);
 
 //send a basic path when you go on the API
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send("Hello before starting you must go on my <a href='https://github.com/isevenbe/Node.JS-MongoDB'>GutHub repository</a>");
 });
 
- // secret variable
+
+
+
+// secret variable
 app.set('superSecret', config.secret);
 
+apiRoutes.use(function (req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded; next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+});
 // choose what port on which to run the server
 const port = process.env.PORT || 3000;
 
 // use the app variable and listen on the port
-app.listen(port , () => {
+app.listen(port, () => {
   console.log(`Server running on *${port}`);
 });
