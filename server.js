0a051f4f746b159dc378e48e9093629547abe774
isevenbe <florentin.lefort@gmail.com>
2018-12-17 11:54:52 +0100

    d

--- server.js
+++ server.js
@@ -28,41 +28,9 @@ app.get('/', function (req, res) {
   res.send("Hello before starting you must go on my <a href='https://github.com/isevenbe/Node.JS-MongoDB'>GutHub repository</a>");
 });
 
-
-
-
 // secret variable
 app.set('superSecret', config.secret);
 
-apiRoutes.use(function (req, res, next) {
-
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  // decode token
-  if (token) {
-
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function (err, decoded) {
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded; next();
-      }
-    });
-
-  } else {
-
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
-    });
-
-  }
-});
 // choose what port on which to run the server
 const port = process.env.PORT || 3000;
 
